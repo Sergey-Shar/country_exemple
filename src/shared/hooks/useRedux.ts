ab9418bf29aa639c4c0950ab7a4466c2a358f96c
Sergey Sharuha <sergeihar82@gmail.com>
2023-10-19 07:18:17 +0300

    add router

--- src/shared/hooks/useRedux.ts
+++
@@ -1,8 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "react-redux";
-import { AppDispatch, RootState } from "../../redux/store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-
-export const useAppStore = useStore<RootState>
